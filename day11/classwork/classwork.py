c535772cf3675418bdbdb0b50d002b6e998a2485
tabuka915 <tabuka2008@gmail.com>
2024-03-14 18:39:35 +0400

    second commit

---
+++ day11/classwork/classwork.py
@@ -0,0 +1,9 @@
+#num = [1,2,3,4,5,6,7,8,9,10]
+#for i in num:
+#    print(num[i]* 12)
+
+products = ["leisi", "cola", "fanta",]
+
+choise = int(input("Enter a num : "))
+
+print(products[choise])
---
+++ day11/homework/homework.py
@@ -0,0 +1,28 @@
+#num=[1,2,3,4,5,6,7,8,9,10]
+
+#for i in num:
+#  print( num [i] * 2)
+#  print(num [i]  + 2)
+#  print(num [i]  / 2 )
+
+
+
+#pets=["cat","dog","frog"]
+
+#pets [2] ='parrot'
+
+#print(pets)
+
+
+soda=["cola","pepsi", "sprite"]
+
+user_choise=2
+
+print(soda[user_choise])
+
+
+
+
+
+
+

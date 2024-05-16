110b1be91560fecd4b7a345ab6e380f3d0c96429
tabuka915 <tabuka2008@gmail.com>
2024-05-16 18:32:35 +0400

    second commit

--- day24/classwork/classwork.py
+++ day24/classwork/classwork.py
@@ -1,4 +1,3 @@
-
-
-
-
+for i in range(1,3):
+  for j in range(2,4):
+    print(i, j)
--- day28/classwork/classwork.py
+++ day28/classwork/classwork.py
@@ -3,6 +3,25 @@ c[:2] = ['₣', '฿']
 print(c)
       
 
-vehicle = 'airplane'
-vehicle[:3] = 'water'
-print(vehicle)     
+cities = ['Berlin', 'Munich']
+print(cities[1])
+
+
+
+
+
+
+
+
+
+
+brands = ["Honda", "Toyota", "BMW", "Mercedes"]
+
+
+print(brands[-3:-1])
+
+products = ['milk', 'eggs', 'apples']
+for i in products:
+  print(i)
+
+

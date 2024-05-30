6606b1e96b3b130eac2f90b931897207c42919cb
tabuka915 <tabuka2008@gmail.com>
2024-05-30 16:58:36 +0400

    second commit

---
+++ day28/homework/homework.py
@@ -0,0 +1,11 @@
+animals = ['cat', 'turtle', 'tiger', 'dog', 'lion']
+for i in animals:
+  if 'r' in i:
+    print(i)
+
+
+animals = ['cat', 'turtle', 'tiger', 'dog', 'lion']
+for i in animals:
+  if 'r' in i:
+    print(i)
+

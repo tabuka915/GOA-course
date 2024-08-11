4d3838e8f48865de9f2e09f0ac1894d97783850d
tabuka915 <tabuka2008@gmail.com>
2024-08-11 19:15:33 +0400

    second commit

---
+++ day55/homework/script.js
@@ -0,0 +1,9 @@
+const p = document.getElementById()
+const button = document.getElementById()
+
+
+let number = 0
+button.addEventListener("sumbit",function(){
+    number = number = 1
+    console.log(number)
+})

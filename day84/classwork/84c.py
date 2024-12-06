6acb0d90c67fcbdb76f873e1eeeb4f0860e71b9f
tabuka915 <tabuka2008@gmail.com>
2024-12-06 16:39:07 +0400

    second commit

--- day84/classwork/84c.py
+++ day84/classwork/84c.py
@@ -1,14 +1,14 @@
 #შევქმნათ ცარიელი მასსივი
-#array = []               
+array = []               
 
 #ჩვენ უნდა შევავსოთ მასსივი 5 სიბოლოთი (ამ კოდში ჩვენ ვირჩევთ რიცხვებს)
-#for i in range(5):
-#    num = int(input(f"Enter integer {i+1}: "))
- #   array.append(num)
+for i in range(5):
+    num = int(input(f"Enter integer {i+1}: "))
+    array.append(num)
 
 #ამ ნაწილში ჩვენ ვწერთ თუ რა გვინდა რომ გამოგვიყვანოს
-#print("Element at 2nd index:", array[2])
-#print("Element at 4th index:", array[4])
+print("Element at 2nd index:", array[2])
+print("Element at 4th index:", array[4])
 
 
 
